fac6dfbe7230a26ff0c6d5f170dd51df2c3c4fa5
Pedro Castro <106095810+peucastro@users.noreply.github.com>
2023-05-17 11:21:07 +0100

    commentd  Image.hpp (2)

--- Image.hpp
+++ Image.hpp
@@ -5,23 +5,32 @@
 namespace prog
 {
     /*
-    represents an image using a bidirectional array, where each 
-    pixel is represented as a Color object (previously implemented)
+    Represents an image using a bidirectional array, where each 
+    pixel is represented as a Color object (previously implemented).
     */
     class Image
     {
     private:
-        int w; //image width variable
-        int h; //image height variable
-        Color **img; //pointer to a bidirectional array of colors (the image itself)
+        // Image width variable.
+        int w;
+        // Image height variable.
+        int h;
+        // Pointer to a bidirectional array of colors (the image itself).
+        Color **img;
 
     public:
-        Image(int w, int h, const Color &fill = {255, 255, 255}); //constructor with arguments
-        ~Image(); //destructor
-        int width() const; //returns the image width
-        int height() const; //returns the image height
-        Color &at(int x, int y); //returns the color at a certain pixel (coordinate)
-        const Color &at(int x, int y) const; //modifies the color at a certain pixel (coordinate)
+        // Constructor with arguments (an image width, height and a color to fill).
+        Image(int w, int h, const Color &fill = {255, 255, 255});
+        // Destructor.
+        ~Image();
+        // Returns the image width.
+        int width() const;
+        // Returns the image height.
+        int height() const;
+        // Returns the color at a certain pixel (coordinate).
+        Color &at(int x, int y);
+        // Modifies the color at a certain pixel (coordinate).
+        const Color &at(int x, int y) const; 
     };
 }
 #endif
