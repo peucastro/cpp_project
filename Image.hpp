0f6f4a50d96d38e0d72386678918a21f9ee53307
Pedro Castro <106095810+peucastro@users.noreply.github.com>
2023-05-17 10:53:04 +0100

    commented  Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -1,24 +1,27 @@
 #ifndef __prog_Image_hpp__
 #define __prog_Image_hpp__
 #include "Color.hpp"
-#include <vector>
 
 namespace prog
 {
+    /*
+    represents an image using a bidirectional array, where each 
+    pixel is represented as a Color object (previously implemented)
+    */
     class Image
     {
     private:
-        int w;
-        int h;
-        Color **img;
+        int w; //image width variable
+        int h; //image height variable
+        Color **img; //pointer to a bidirectional array of colors (the image itself)
 
     public:
-        Image(int w, int h, const Color &fill = {255, 255, 255});
-        ~Image();
-        int width() const;
-        int height() const;
-        Color &at(int x, int y);
-        const Color &at(int x, int y) const;
+        Image(int w, int h, const Color &fill = {255, 255, 255}); //constructor with arguments
+        ~Image(); //destructor
+        int width() const; //returns the image width
+        int height() const; //returns the image height
+        Color &at(int x, int y); //returns the color at a certain pixel (coordinate)
+        const Color &at(int x, int y) const; //modifies the color at a certain pixel (coordinate)
     };
 }
 #endif
