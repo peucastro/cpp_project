33ebd2fb99816b0e20f58ee5989f34cc3b7ec585
Sofiabl04 <131355573+Sofiabl04@users.noreply.github.com>
2023-05-18 12:11:47 +0100

    Update Script.hpp

--- Script.hpp
+++ Script.hpp
@@ -7,7 +7,7 @@
 
 namespace prog
 {
-    
+    // Representation of a commands manipulated on an image.
     class Script
     {
     public:
@@ -27,15 +27,25 @@ namespace prog
         void open();
         void blank();
         void save();
+        // void function to invert each pixel in the image.
         void invert();
+        // void function to transform the color of the image into grey.
         void to_gray_scale();
+        //void function to switch the color of the image.
         void replace();
+        // void function to fill the area with the specific color.
         void fill();
+        // void function to invert the pictire horizontally.
         void h_mirror();
+        // void function to invert the pictire vertically.
         void v_mirror();
+        // void function to add the file image to a certain position(x,y).
         void add();
+        // void function to reduce the size of the image.
         void crop();
+        // void function to rotate the image to the left.
         void rotate_left();
+        // void function to rotate the image to the right.
         void rotate_right();
     };
 }
